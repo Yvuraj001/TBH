91f663a38c9b2a2fac2fc91199b33fcb77bff51a
Yuvraj <yuvraj@Yuvrajs-MacBook-Air.local>
2026-06-23 20:44:48 +0530

    finished navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,21 @@
-import React from "react";
-
 const Navbar = () => {
   return (
-    <nav className="flex justify-between px-5 py-2 mx-6 my-4 sticky top-2 z-4  ">
-      <div className="font-modak text-red-500 text-5xl [-webkit-text-stroke:5px_white] [paint-order:stroke]">
+    <nav className="navbar flex justify-between px-5 py-2 mx-6  sticky top-0 backdrop-blur-[3px] border-2 border-yellow-600 bg-[#ffa31f78] rounded-b-4xl items-center shadow-xl ">
+      <a
+        href="/"
+        className="font-modak text-red-500 text-3xl md:text-5xl [-webkit-text-stroke:5px_white] [paint-order:stroke] cursor-pointer"
+      >
         TBH
-      </div>
-      <ul className="flex-center gap-7 text-white">
+      </a>
+      <ul className="flex-center sm:gap-7 gap-3 text-white">
         <li>
           <a href="#burger">
-            <button className="bg-red-500 text-white px-5 py-2.5 rounded-3xl cursor-pointer    hover:bg-black transition-all ease-in duration-150 hover:transform hover:scale-[1.09]">
-              Burgers
-            </button>
+            <button className="burger-btn">Burgers</button>
           </a>
         </li>
         <li>
-          <a href="#hero">
-            <button className="bg-transparent text-black px-6 py-2 rounded-3xl cursor-pointer border-2 border-[#ceb79c] hover:border-[#000000] transition-all ease-in duration-150 hover:transform hover:scale-[1.09]">
+          <a href="#menu">
+            <button className="menu-btn">
               <span className="text-sm">MENU</span>
             </button>
           </a>
