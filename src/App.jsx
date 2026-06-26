1cdebb4b1e656597faaceea4f7452c69d41bf423
Yuvraj <yuvraj@Yuvrajs-MacBook-Air.local>
2026-06-26 19:15:18 +0530

    added info.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import DeviceBlock from "./components/DeviceBlock";
+import Info from "./components/Info";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <main className="w-screen h-screen bg-[#ffbb7887] overflow-x-hidden">
         <Navbar />
         <Hero />
+        <Info/>
       </main>
     </DeviceBlock>
   );
